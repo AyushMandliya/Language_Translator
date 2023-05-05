d3c8fc56371f18f05802f3f9f28446da8d107849
ayush <ayush.raj2019@outlook.com>
2023-05-05 23:45:20 +0530

    code

--- app/src/main/java/com/example/languagetranslator/MainActivity.java
+++ app/src/main/java/com/example/languagetranslator/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.languagetranslator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner fromSpinner,toSpinner;
+    private TextInputEditText sourceEdt;
+    private ImageView micIV;
+    private MaterialButton translateBtn;
+    private TextView translatedTV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fromSpinner=findViewById(R.id.idFromSpinner);
+        toSpinner=findViewById(R.id.idToSpinner);
+        sourceEdt=findViewById(R.id.idEdtSource);
+        micIV=findViewById(R.id.idIVMic);
+        translateBtn=findViewById(R.id.IdBtnTranslate);
+        translatedTV=findViewById(R.id.idTVTranslatedTV)
+
+
     }
 }
